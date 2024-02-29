8d8ac7424ad04d71e6ff4b931f9bac32ee3c4290
rklad <525rkladerman@frhsd.com>
2024-02-29 13:46:54 -0500

    determined winner for each round & got buttons to work, need to work on scoreboard

--- script.js
+++ script.js
@@ -1,68 +1,152 @@
-Window.onload = changeUsername();
-
 //GETTING USERNAME
-function changeUsername(element) {
-  var username = prompt("Enter your username:");
-  document.getElementById("username1").innerText = "Welcome " + username + " to Kendrick, Drake, Cole, a HipHop version of Rock Paper Scissors! Select one of Kendrick Lamar, Drake, or J Cole and try to beat the computer in a best-of-5! Kendrick beats Drake, Drake beats Cole, and Cole beats Kendrick. Good Luck!";
-  document.getElementById("username2").innerText = username + "'s choice";
-  document.getElementById("username3").innerText = username;
-  // add a 'please enter your name' for null
-}
+  let username = prompt("Enter your username:");
+  let names = `${username}`
+  document.getElementById("username1").innerText = `Welcome  ${names} to Kendrick, Drake, Cole, a HipHop version of Rock Paper Scissors! Select one of Kendrick Lamar, Drake, or J Cole and try to beat the computer in a best-of-5! 
+  
+  Kendrick beats Drake, Drake beats Cole, and Cole beats Kendrick. Good Luck!`;
+  document.getElementById("username2").innerText = `${names}'s choice`;
+  document.getElementById("username3").innerText = `${names}`;
+  let userScore = 0;
+  let compScore = 0;
 
-
+// THE ENTIRE FUNCTION
 function playGame(userChoice) {
-  // get user's choice
-
-
-
-
-  // display user choice
-
-
-}
-
-
-
-
-// get computer choice
-
-let compChoice = getComputerChoice()
-
+  //COMP CHOICE
+  let compChoice = getComputerChoice()
   function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
       case 0:
-        return "kendrick";
+        return "Kendrick";
         break;
       case 1:
-        return "drake";
+        return "Drake";
         break;
       case 2:
-        return "cole";
+        return "Cole";
         break;
     } 
 
-    // display computer choice
-    if (compChoice === 'kendrick') {
-        document.getElementById('image2').src = 'imgs/kendrick.webp'
-    } else if (compChoice === 'drake') {
-        document.getElementById('image2').src = 'imgs/drake3.webp'
-    } else if (compChoice === 'cole') {
-        document.getElementById('image2').src = 'imgs/cole3.webp'
-    }
-
-    // FOR SMALL IMGS
-    if (randomNumber === 'kendrick') {
-      document.getElementById('smallimage2').src = 'imgs/kendrick4.jpg'
-  } else if (randomNumber === 'drake') {
-      document.getElementById('smallimage2').src = 'imgs/drake4.png'
-  } else if (randomNumber === 'cole') {
-      document.getElementById('smallimage2').src = 'imgs/cole4.jpg'
+    
   }
 
-    //need to display computer choice
-  }
-  // need to display user choice
+  // get user's choice
+  // WHEN USER CHOOSES KENDRICK & COMP CHOOSES DRAKE
+if (userChoice === 'Kendrick' && compChoice === 'Drake') {
+  userScore();
+  document.getElementById('user').src = 'imgs/kendrick.webp'; 
+  document.getElementById('comp').src = 'imgs/drake3.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/kendrick4.jpg';
+  document.getElementById('smallcomp').src = 'imgs/drake4.png' ;
 
+  document.getElementById('decider').innerHTML = `${names} chose ${userChoice}, Computer chose ${compChoice}, ${names} wins!`
+} 
+
+// WHEN USER CHOOSES KENDRICK & COMP CHOOSES COLE
+  else if (userChoice === 'Kendrick' && compChoice === 'Cole') {
+  compScore();
+  document.getElementById('user').src = 'imgs/kendrick.webp'; 
+  document.getElementById('comp').src = 'imgs/cole3.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/kendrick4.jpg';
+  document.getElementById('smallcomp').src = 'imgs/cole4.jpg';
+
+  document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, Computer wins :(`
+} 
+
+  // WHEN USER CHOOSES KENDRICK & COMP CHOOSES KENDRICK
+  else if (userChoice === 'Kendrick' && compChoice === 'Kendrick') {
+  document.getElementById('user').src = 'imgs/kendrick.webp'; 
+  document.getElementById('comp').src = 'imgs/kendrick.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/kendrick4.jpg';
+  document.getElementById('smallcomp').src = 'imgs/kendrick4.jpg';
+
+  document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, it's a Tie!`
+} 
+
+  // WHEN USER CHOOSES DRAKE & COMP CHOOSES DRAKE
+  else if (userChoice === 'Drake' && compChoice === 'Drake') {
+  document.getElementById('user').src = 'imgs/drake3.webp'; 
+  document.getElementById('comp').src = 'imgs/drake3.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/drake4.png';
+  document.getElementById('smallcomp').src = 'imgs/drake4.png';
+
+  document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, it's a Tie!`
+} 
+
+  // WHEN USER CHOOSES DRAKE AND COMP CHOOSES KENDRICK
+  else if (userChoice === 'Drake' && compChoice === 'Kendrick') {
+  compScore();  
+  document.getElementById('user').src = 'imgs/drake3.webp'; 
+  document.getElementById('comp').src = 'imgs/kendrick.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/drake4.png';
+  document.getElementById('smallcomp').src = 'imgs/kendrick4.jpg';
+
+  document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, Computer wins :(`
+} 
+
+  // WHEN USER CHOOSES DRAKE AND COMP CHOOSES COLE
+  else if (userChoice === 'Drake' && compChoice === 'Cole') {
+  userScore();
+  document.getElementById('user').src = 'imgs/drake3.webp'; 
+  document.getElementById('comp').src = 'imgs/cole3.webp';
+  //FOR SMALL IMGS
+  document.getElementById('smalluser').src = 'imgs/drake4.png';
+  document.getElementById('smallcomp').src = 'imgs/cole4.jpg';
+
+  document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, ${username} wins!`
+} 
+
+  // WHEN USER CHOOSES COLE AND COMP CHOOSES COLE 
+  else if (userChoice === 'Cole' && compChoice === 'Cole') {
+    document.getElementById('user').src = 'imgs/cole3.webp'; 
+    document.getElementById('comp').src = 'imgs/cole3.webp';
+    //FOR SMALL IMGS
+    document.getElementById('smalluser').src = 'imgs/cole4.jpg';
+    document.getElementById('smallcomp').src = 'imgs/cole4.jpg';
+
+    document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, it's a Tie!`
+}
+
+  // WHEN USER CHOOSES COLE AND COMP CHOOSES KENDRICK
+  else if (userChoice === 'Cole' && compChoice === 'Kendrick') {
+    userScore();
+    document.getElementById('user').src = 'imgs/cole3.webp'; 
+    document.getElementById('comp').src = 'imgs/kendrick.webp';
+    //FOR SMALL IMGS
+    document.getElementById('smalluser').src = 'imgs/cole4.jpg';
+    document.getElementById('smallcomp').src = 'imgs/kendrick4.jpg';
+
+    document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, ${username} wins!`
+  }
+
+  // WHEN USER CHOOSES COLE AND COMP CHOOSES DRAKE 
+  else if (userChoice === 'Cole' && compChoice === 'Drake') {
+    compScore();
+    document.getElementById('user').src = 'imgs/cole3.webp'; 
+    document.getElementById('comp').src = 'imgs/drake3.webp';
+    //FOR SMALL IMGS
+    document.getElementById('smalluser').src = 'imgs/cole4.jpg';
+    document.getElementById('smallcomp').src = 'imgs/drake4.png';
+
+    document.getElementById('decider').innerText = `${names} chose ${userChoice}, Computer chose ${compChoice}, Computer wins :(`
+  }
+
+
+  //DETERMINING USER SCORE 
+  function userScore() {
+    userScore++;
+    document.getElementById('userscoreboard').innerText = userScore
+  }
+  //DETERMINING COMP SCORE
+  function compScore() {
+    compScore++;
+    document.getElementById('compscoreboard').innerText = compScore
+  }
+}
 
 
